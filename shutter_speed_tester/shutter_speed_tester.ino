31e18efd51f7f4eeda500fb46db89c3d349d7b3d
Izsó András <izso.andras@simonyi.bme.hu>
2022-07-22 16:55:28 +0200

    Init commit

---
+++ shutter_speed_tester/shutter_speed_tester.ino
@@ -0,0 +1,13 @@
+#define LIGHT_PIN A7
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LIGHT_PIN, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
